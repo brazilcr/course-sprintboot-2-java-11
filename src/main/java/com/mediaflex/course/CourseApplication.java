e407569e11a6f7a4400f2c9cefcadad98a81b7d3
Cláudio Baima <baimaclaudio@gmail.com>
2021-09-06 17:32:36 -0300

    H2 database, test profile, JPA

--- src/main/java/com/mediaflex/course/CourseApplication.java
+++ src/main/java/com/mediaflex/course/CourseApplication.java
@@ -1,4 +1,4 @@
-package com.mediaflexcourse;
+package com.mediaflex.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/mediaflex/course/resources/UserResource.java
+++ src/main/java/com/mediaflex/course/resources/UserResource.java
@@ -1,11 +1,11 @@
-package com.mediaflexcourse.resources;
+package com.mediaflex.course.resources;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mediaflexcourse.entities.User;
+import com.mediaflex.course.entities.User;
 
 @RestController
 @RequestMapping(value = "/users")
