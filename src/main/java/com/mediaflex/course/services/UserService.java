7388d20d7737c1089b6999b37f2c74823790a409
Cláudio Baima <baimaclaudio@gmail.com>
2021-09-06 18:37:17 -0300

    Service layer, component registration

---
+++ src/main/java/com/mediaflex/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.mediaflex.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mediaflex.course.entities.User;
+import com.mediaflex.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired 
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	public User findById(Long id) {
+		Optional<User> obj  = repository.findById(id);
+		return obj.get();
+	}
+
+}
+
