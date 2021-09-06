7cd840a16616dcce3c4f262bcef649517b1b720c
Cláudio Baima <baimaclaudio@gmail.com>
2021-09-06 17:50:08 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/mediaflex/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.mediaflex.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mediaflex.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+}
