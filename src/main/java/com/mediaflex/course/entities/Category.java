9f5897ad5c3b2ec88d676fd844b0c91479c6400d
Cláudio Baima <baimaclaudio@gmail.com>
2021-09-07 14:02:31 -0300

    Product

--- src/main/java/com/mediaflex/course/entities/Category.java
+++ src/main/java/com/mediaflex/course/entities/Category.java
@@ -1,8 +1,11 @@
 package com.mediaflex.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
+import javax.persistence.Transient;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,6 +21,9 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Transient 
+	private Set<Product> products = new HashSet<>();
 
 	public Category() {
 	}
@@ -38,6 +44,10 @@ public class Category implements Serializable {
 		this.id = id;
 		this.name = name;
 	}
+	public Set<Product> getProducts() {
+		return products;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -53,5 +63,4 @@ public class Category implements Serializable {
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
-
 }
---
+++ src/main/java/com/mediaflex/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.mediaflex.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mediaflex.course.entities.Product;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+}
