89e6e56f1fd69534d3146d2c44024f19e9a6294b
Cláudio Baima <baimaclaudio@gmail.com>
2021-09-07 14:47:08 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/mediaflex/course/entities/Category.java
+++ src/main/java/com/mediaflex/course/entities/Category.java
@@ -5,12 +5,14 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-import javax.persistence.Transient;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_category")
@@ -22,7 +24,9 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
 	
-	@Transient 
+	// @Transient
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 
 	public Category() {
--- src/main/java/com/mediaflex/course/entities/Product.java
+++ src/main/java/com/mediaflex/course/entities/Product.java
@@ -8,15 +8,17 @@ import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Transient;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_product")
-public class Product  implements Serializable {
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -24,9 +26,13 @@ public class Product  implements Serializable {
 	private String description;
 	private Double price;
 	private String imgUrl;
+
+	// o set é uma interface (não pode ser instancia)... utiliza uma classe
+	// equivalente a interface
+	// @Transient para o JPA não reconhecer
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	
-	// o set é uma interface (não pode ser instancia)... utiliza uma classe equivalente a interface
-	@Transient
 	private Set<Category> categories = new HashSet<>();
 
 	public Product() {
@@ -102,5 +108,5 @@ public class Product  implements Serializable {
 		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 }
