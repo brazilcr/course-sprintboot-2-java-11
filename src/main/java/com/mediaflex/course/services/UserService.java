b44e92ccc527dc832abfa8015f90be84c914bf51
Cláudio Baima <baimaclaudio@gmail.com>
2021-09-08 17:24:04 -0300

    Exception handling - update

--- src/main/java/com/mediaflex/course/services/UserService.java
+++ src/main/java/com/mediaflex/course/services/UserService.java
@@ -3,6 +3,8 @@ package com.mediaflex.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -15,45 +17,56 @@ import com.mediaflex.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
-	
-	@Autowired 
+
+	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
+
 	public User findById(Long id) {
-		Optional<User> obj  = repository.findById(id);
-		return obj.orElseThrow( () -> new ResourceNotFoundException(id) );
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		// usar metodo no repository.. save... por padrão retorna o objeto salvo
 		return repository.save(obj);
 	}
+
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
 			// capturou exeption e lancou o padrão de recursos inexistentes
 			throw new ResourceNotFoundException(id);
-		} catch( DataIntegrityViolationException e ) {
-			throw new DatabaseException( e.getMessage() );
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
 		} catch (RuntimeException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch( EntityNotFoundException e ) {
+			throw new ResourceNotFoundException( id );
+		}
+//		} catch (RuntimeException e) {
+//			e.printStackTrace();
+//			throw new ResourceNotFoundException(id);
+//		}
+//			
 	}
-	
+
+
 	private void updateData(User entity, User obj) {
-		entity.setName(obj.getName() );
-		entity.setEmail( obj.getEmail() );
-		entity.setPhone( obj.getPhone() );
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
-
